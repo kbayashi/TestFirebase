f8dd52328585b988b82d1d4ffe1676df2e52af95
kuribayashi228 <228kuri@gmail.com>
2020-06-23 15:58:38 +0900

    アカウント作成&データベースにユーザ情報を保存成功

---
+++ app/src/main/java/com/example/testfirebase/User.kt
@@ -0,0 +1,46 @@
+package com.example.testfirebase
+
+import android.util.Log
+import java.lang.IllegalArgumentException
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class User (val Uid:String, val Name:String, val Img:String,
+            val Gender:String, val Birthday: String, val PR:String,
+            val Live: String, val Sick:String, val LifeExpectancy: String){
+
+    constructor():this("", "", "", "",
+        "", "", "", "",
+        "")
+
+
+    //年齢算出
+    fun ageCalculation(birthday:String): String {
+
+        val now = Calendar.getInstance()
+        val birthdayDate = birthday.toDate()
+        val birthdayCalender = Calendar.getInstance()
+        birthdayCalender.time = birthdayDate
+
+        var age: Int = (now.get(Calendar.YEAR) - birthdayCalender.get(Calendar.YEAR))
+        if(now.get(Calendar.DAY_OF_YEAR) < birthdayCalender.get(Calendar.DAY_OF_YEAR)) {
+            age -= 1
+        }
+
+        Log.d("Test", age.toString())
+
+        return age.toString()
+    }
+
+    private fun String.toDate(pattern: String = "yyyy/MM/dd"): Date?{
+        return try{
+            SimpleDateFormat(pattern).parse(this)
+        }catch (e: IllegalArgumentException){
+            return null
+        }catch (e: ParseException){
+            return null
+        }
+    }
+
+}
--- app/src/main/java/com/example/testfirebase/UserRegistarActivity.kt
+++ app/src/main/java/com/example/testfirebase/UserRegistarActivity.kt
@@ -1,12 +1,80 @@
 package com.example.testfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_user_registar.*
 
 class UserRegistarActivity : AppCompatActivity() {
+
+
+    var ragioGender:RadioButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_registar)
+
+
+        //登録ボタン
+        user_registar_button.setOnClickListener {
+            ContentInspection()
+        }
+
+        //ラジオボタンタップ
+        user_registar_gender_radioGroup.setOnCheckedChangeListener { radioGroup, i ->
+                ragioGender = findViewById<RadioButton>(i)
+                Log.d("ラジオグループ", ragioGender?.text.toString())
+            }
+    }
+
+    //入力検査(まだ適当)と認証
+    private fun ContentInspection(){
+
+        val name = user_registar_name_editText.text.toString()
+        val email = user_registar_email_editText.text.toString()
+        val pass = user_registar_pass_editText.text.toString()
+        val checkPass = user_registar_pass_check_editText.text.toString()
+
+        if(email.isEmpty() || pass.isEmpty() || name.isEmpty()){
+            Toast.makeText(this, "入力内容が不足しています", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pass)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+                //認証
+                Log.d("認証", "認証成功 ${it.result?.user?.uid}")
+                val user = User(it.result?.user?.uid!!, name, "", ragioGender?.text.toString(),
+                    "", "", "","", "")
+                UserSaveDB(user)
+            }
+            .addOnFailureListener {
+                Log.d("Main", "作成に失敗しました ${it.message}")
+                Toast.makeText(this, "作成に失敗しました ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+    //ユーザのデータをデータベースに登録
+    private fun UserSaveDB(user:User){
+        val ref  = FirebaseFirestore.getInstance().collection("user")
+            .document(user.Uid)
+
+        ref.set(user)
+            .addOnSuccessListener {
+                Log.d("データベース", "データベースに保存成功 ")
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Log.d("データベース", "データベースに保存失敗 ${it.message}")
+            }
     }
 }
