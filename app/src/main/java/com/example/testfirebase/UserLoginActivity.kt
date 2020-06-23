75c2c8fcb64a52eb7c52ff3e7285409c109c4cd7
Takashichaso <username2180130@gmail.com>
2020-06-23 12:12:51 +0900

    ユーザーログイン画面のデザインができました。

---
+++ app/src/main/java/com/example/testfirebase/UserLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_login)
+    }
+}
