2a737eb43fd2d17cf2d025c5c08fb70bbda905fb
Takashichaso <username2180130@gmail.com>
2020-06-26 10:59:27 +0900

    ログイン確認できました。

--- app/src/main/java/com/example/testfirebase/UserLoginActivity.kt
+++ app/src/main/java/com/example/testfirebase/UserLoginActivity.kt
@@ -1,11 +1,40 @@
 package com.example.testfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_user_login.*
+
 
 class UserLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+
+        var email:String = "";
+        var password:String = "";
+
+        val toastButton: Button = findViewById(R.id.user_login_login_Button)
+        toastButton.setOnClickListener {
+            email = user_login_user_mail_editText.text.toString()
+            password = user_login_user_pass_editText.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful){
+                        Log.d("ログインできません", "aaaa")
+                        return@addOnCompleteListener
+                    }
+                    Log.d("ログイン" ,"ログイン成功")
+                    val intent = Intent(this, MainActivity::class.java)
+                    //アクティビティを起動する前に既存のアクティビティを削除してからスタックに追加
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+        }
+
     }
 }
