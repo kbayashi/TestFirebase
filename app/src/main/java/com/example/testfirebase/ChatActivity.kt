5ff1ed8a8d48d6911930e7c2455786d8f703b5d7
obakyuu <maedahiroki0521@gmail.com>
2020-06-29 18:17:04 +0900

    Fontを削除

--- app/src/main/java/com/example/testfirebase/ChatActivity.kt
+++ app/src/main/java/com/example/testfirebase/ChatActivity.kt
@@ -4,10 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ChatActivity : AppCompatActivity() {
+    companion object{
+        val SELECT_USER = "SELECT_USER"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        supportActionBar?.title = "前田裕輝"
+        val intent = getIntent()
+        supportActionBar?.title = intent.getStringExtra("SELECT_USER")
+
+        //val adapter = GroupAdapter<ViewHolder>()
+
+        //adapter.add(ChatItem())
+        //adapter.add(ChatItem())
+        //adapter.add(ChatItem())
+        //adapter.add(ChatItem())
+
+        //chat_recyclerView.adapter = adapter
     }
 }
+
+/*
+class ChatItem: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+
+    }
+
+    override fun getLayout():Int{
+        return R.layout.chat_me_row
+    }
+}
+ */
