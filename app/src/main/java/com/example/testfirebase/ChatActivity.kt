331aac8134a152375cdee2bdf8173ef313069173
obakyuu <maedahiroki0521@gmail.com>
2020-06-29 12:25:25 +0900

    チャット画面の下部にメッセージ入力欄、送信ボタンを配置。

--- app/src/main/java/com/example/testfirebase/ChatActivity.kt
+++ app/src/main/java/com/example/testfirebase/ChatActivity.kt
@@ -7,5 +7,7 @@ class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        supportActionBar?.title
     }
 }
