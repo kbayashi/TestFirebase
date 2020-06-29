099e55992a6313cd344c706c76c3aa4f3aba2860
kbayashi <228kuri@gmail.com>
2020-06-29 15:43:27 +0900

    登録画面のレイアウトを

--- app/src/main/java/com/example/testfirebase/UserRegistarActivity.kt
+++ app/src/main/java/com/example/testfirebase/UserRegistarActivity.kt
@@ -1,16 +1,16 @@
 package com.example.testfirebase
 
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_user_registar.*
+
 
 class UserRegistarActivity : AppCompatActivity() {
 
@@ -20,7 +20,6 @@ class UserRegistarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_registar)
-
 
         //登録ボタン
         user_registar_button.setOnClickListener {
