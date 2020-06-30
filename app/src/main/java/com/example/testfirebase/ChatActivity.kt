b3806c08944f3a1baf696b1ffe1ce982c4d1c581
obakyuu <maedahiroki0521@gmail.com>
2020-06-30 10:39:23 +0900

    ボタンイベントを設定。ここからメッセージデータベースに追加とかを行う

--- app/src/main/java/com/example/testfirebase/ChatActivity.kt
+++ app/src/main/java/com/example/testfirebase/ChatActivity.kt
@@ -3,6 +3,7 @@ package com.example.testfirebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.testfirebase.UserListFragment.Companion.SELECT_USER
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
 
@@ -14,25 +15,9 @@ class ChatActivity : AppCompatActivity() {
         val get_username = intent.getParcelableExtra<User>(SELECT_USER)
         supportActionBar?.title = get_username.name
 
-        //val adapter = GroupAdapter<ViewHolder>()
+        send_button.setOnClickListener {
 
-        //adapter.add(ChatItem())
-        //adapter.add(ChatItem())
-        //adapter.add(ChatItem())
-        //adapter.add(ChatItem())
+        }
 
-        //chat_recyclerView.adapter = adapter
     }
 }
-
-/*
-class ChatItem: Item<ViewHolder>(){
-    override fun bind(viewHolder: ViewHolder, position: Int){
-
-    }
-
-    override fun getLayout():Int{
-        return R.layout.chat_me_row
-    }
-}
- */
