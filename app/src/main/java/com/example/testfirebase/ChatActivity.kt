540fe42bd312b89c7263a6a10b30054cf1c3a008
obakyuu <maedahiroki0521@gmail.com>
2020-07-03 12:29:48 +0900

    メッセージ送信関数を作っている最中に燃え尽きた

--- app/src/main/java/com/example/testfirebase/ChatActivity.kt
+++ app/src/main/java/com/example/testfirebase/ChatActivity.kt
@@ -1,10 +1,16 @@
 package com.example.testfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.testfirebase.UserListFragment.Companion.SELECT_USER
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_chat.*
+
+private lateinit var auth: FirebaseAuth
 
 class ChatActivity : AppCompatActivity() {
 
@@ -12,17 +18,39 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
+        //下のオブジェクトは相手のオブジェクト情報です。
         val intent = getIntent()
-        val get_name = intent.getParcelableExtra<User>(SELECT_USER)
-        supportActionBar?.title = get_name.name
+        val get_you = intent.getParcelableExtra<User>(SELECT_USER)
+        //アクションバーに相手の名前を表記
+        supportActionBar?.title = get_you.name
 
         send_button.setOnClickListener {
             if(message_editText.text.isEmpty()){
                 Toast.makeText(applicationContext, "メッセージを入力してください", Toast.LENGTH_SHORT).show()
             }else{
-
+                sendMessage(get_you,message_editText.text.toString())
             }
         }
 
+    }
+
+    // Firebaseの「user-message」コレクションにオブジェクトを登録する関数
+    // 引数: you(チャット相手のユーザオブジェクト) / msg(送信するメッセージ内容)
+    private fun sendMessage(you:User,msg:String){
+        //自分のユーザ情報を取得
+        auth = FirebaseAuth.getInstance()
+        val me = auth.currentUser
+        //データベースにメッセージを登録
+        val ref  = FirebaseFirestore.getInstance().collection("user-message")
+            .document(me!!.uid)
+        ref.set(me!!.uid)
+            .addOnSuccessListener {
+                Log.d("データベース", "データベースにメッセージを登録しました")
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Log.d("データベース", "データベースにメッセージの登録が失敗しました ${it.message}")
+            }
     }
 }
