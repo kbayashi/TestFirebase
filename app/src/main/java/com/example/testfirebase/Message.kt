310d3cd7490d06f7937bbf2f54296e4c4ddee854
kbayashi <228kuri@gmail.com>
2020-07-08 18:13:01 +0900

    保存されいる日数によって表示を変更する処理を追加

--- app/src/main/java/com/example/testfirebase/Message.kt
+++ app/src/main/java/com/example/testfirebase/Message.kt
@@ -8,8 +8,18 @@ class Message(val message: String,val send_user:String,val receive_user: String,
     constructor():this("", "", "", -1L)
 
     fun changeTimeStampToString(long:Long): String{
+
+        var format = SimpleDateFormat("h:mm")
+        val today = System.currentTimeMillis()
+
+
+        if(long < (today - 172800000L)){
+            format = SimpleDateFormat("M/d")
+        }else if(long < (today - 86400000L)){
+            return "昨日"
+        }
+
         val date = Date(long)
-        val format = SimpleDateFormat("MM月dd日 hh時mm分")
         return format.format(date)
     }
 
