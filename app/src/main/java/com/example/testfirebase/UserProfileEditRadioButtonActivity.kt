c0bb6fd76f6c5168ed62399a7b9c434348e45b50
HuumaKotarou <takeda.gakushuu@gmail.com>
2020-09-04 11:37:26 +0900

    性別編集画面の不具合修正

--- app/src/main/java/com/example/testfirebase/UserProfileEditRadioButtonActivity.kt
+++ app/src/main/java/com/example/testfirebase/UserProfileEditRadioButtonActivity.kt
@@ -2,6 +2,8 @@ package com.example.testfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.RadioButton
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_user_profile_edit_radio_button.*
 
@@ -10,15 +12,15 @@ class UserProfileEditRadioButtonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile_edit_radio_button)
-        val man = activity_user_profile_edit_radio_button_man_radioButton.isChecked
-        val woman = activity_user_profile_edit_radio_button_woman_radioButton.isChecked
+        val man = findViewById<RadioButton>(R.id.activity_user_profile_edit_radio_button_man_radioButton)
+        val woman = findViewById<RadioButton>(R.id.activity_user_profile_edit_radio_button_woman_radioButton)
 
         activity_user_profile_edit_radio_button_save_button.setOnClickListener{
-            if(man == true){
-                db
+            if(man.isChecked == true){
+
                 finish()
-            }else if(woman == true){
-                db
+            }else if(woman.isChecked == true){
+
                 finish()
             }
         }
