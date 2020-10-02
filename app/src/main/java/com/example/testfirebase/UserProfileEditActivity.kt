5c1340f8b750461660d729052c64ba334a8b0d3e
HuumaKotarou <takeda.gakushuu@gmail.com>
2020-10-02 13:48:50 +0900

    ユーザープロフィール編集画面のUIへんこー

--- app/src/main/java/com/example/testfirebase/UserProfileEditActivity.kt
+++ app/src/main/java/com/example/testfirebase/UserProfileEditActivity.kt
@@ -14,53 +14,61 @@ class UserProfileEditActivity : AppCompatActivity() {
         val intent = getIntent()
         val get_user = intent.getParcelableExtra<User>(SELECT_USER)
 
-        user_profile_edit_name_textView.text = "名前:"+get_user.name
-        user_profile_edit_pr_textView.text = "自己紹介:"+get_user.pr
-        user_profile_edit_gender_textView.text = "性別:"+get_user.gender
-        user_profile_edit_old_textView.text = "年齢:"+get_user.birthday
-        user_profile_edit_prefectures_textView.text = "都道府県:"+get_user.live
-        user_profile_edit_hobby_textView.text = "趣味:非公開"
-        user_profile_edit_sick_textView.text = "病名:"+get_user.sick
-        user_profile_edit_lifeexpectancy_textView.text = "余命:"+get_user.life_expectancy
+        user_profile_edit_user_name_textView.text = get_user.name
+        user_profile_edit_name_textView.text = "名前"
+        user_profile_edit_user_pr_textView.text = get_user.pr
+        user_profile_edit_pr_textView.text = "自己紹介"
+        user_profile_edit_user_gender_textView.text = get_user.gender
+        user_profile_edit_gender_textView.text = "性別"
+        user_profile_edit_user_old_textView.text = get_user.birthday
+        user_profile_edit_old_textView.text = "生年月日"
+        user_profile_edit_user_prefectures_textView.text = get_user.live
+        user_profile_edit_prefectures_textView.text = "都道府県"
+        user_profile_edit_user_hobby_textView.text = "非公開"
+        user_profile_edit_hobby_textView.text = "趣味"
+        user_profile_edit_user_sick_textView.text = get_user.sick
+        user_profile_edit_sick_textView.text = "病名"
+        user_profile_edit_user_lifeexpectancy_textView.text = get_user.life_expectancy
+        user_profile_edit_lifeexpectancy_textView.text = "余命"
         Picasso.get().load(get_user.img).into(user_profile_edit_imageview)
 
-        user_profile_edit_name_textView.setOnClickListener{
+        name_relativelayout.setOnClickListener{
             val intent = Intent(this,UserProfileEditPlainTextActivity::class.java)
             intent.putExtra("table", "name")
                 .putExtra("edit",get_user.name)
             startActivity(intent)
         }
-        user_profile_edit_pr_textView.setOnClickListener{
+        pr_relativelayout.setOnClickListener{
             val intent = Intent(this,UserProfileEditPlainTextActivity::class.java)
             intent.putExtra("table","pr")
                 .putExtra("edit",get_user.pr)
             startActivity(intent)
         }
-        user_profile_edit_gender_textView.setOnClickListener{
+        gender_relativelayout.setOnClickListener{
             val intent = Intent(this,UserProfileEditRadioButtonActivity::class.java)
             intent.putExtra("table","gender")
                 .putExtra("edit",get_user.gender)
             startActivity(intent)
         }
-        user_profile_edit_old_textView.setOnClickListener{
+        old_relativelayout.setOnClickListener{
             val intent = Intent(this,UserProfileEditSpinnerActivity::class.java)
             intent.putExtra("table","old")
                 .putExtra("edit",get_user.birthday)
             startActivity(intent)
         }
-        user_profile_edit_prefectures_textView.setOnClickListener{
+        prefecture_relativelayout.setOnClickListener{
             val intent = Intent(this,UserProfileEditSpinnerActivity::class.java)
             intent.putExtra("table","live")
                 .putExtra("edit",get_user.live)
             startActivity(intent)
         }
-        user_profile_edit_sick_textView.setOnClickListener {
+        sick_relativelayout.setOnClickListener {
             val intent = Intent(this,UserProfileEditSpinnerActivity::class.java)
             intent.putExtra("table","sick")
                 .putExtra("edit",get_user.sick)
             startActivity(intent)
         }
-        user_profile_edit_lifeexpectancy_textView.setOnClickListener {
+        lifeexpectancy_relativelayout.setOnClickListener {
             val intent = Intent(this,UserProfileEditSpinnerActivity::class.java)
             intent.putExtra("table","lifeexpectancy")
                 .putExtra("edit",get_user.life_expectancy)
