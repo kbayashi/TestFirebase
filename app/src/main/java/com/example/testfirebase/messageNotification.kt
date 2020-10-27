fa401ba3361882cdb1e1ac083ea82c90b81c5caa
HuumaKotarou <66808082+huumakotarou@users.noreply.github.com>
2020-10-27 12:35:00 +0900

    討死 うんち トークンをfirebaseに送信したいがやり方がわからずググっても何も出てこないゴミカス

--- app/src/main/java/com/example/testfirebase/messageNotification.kt
+++ app/src/main/java/com/example/testfirebase/messageNotification.kt
@@ -6,28 +6,51 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.media.RingtoneManager
 import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationCompat.PRIORITY_MAX
+import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import java.util.*
 
 class PushNotificationListenerService: FirebaseMessagingService() {
 
+    private val TAG = javaClass.simpleName
+
     // 新しいトークンが生成された時の処理
-    // 中でサーバにトークンを送信する処理などを定義
+    // サーバにトークンを送信する処理などを定義
     override fun onNewToken(p0: String) {
         super.onNewToken(p0)
+        Log.d(TAG,"onNewToken token:$p0")
 
+        //サーバにトークンを送信する
+        sendRegistrationToServer(p0)
         // チャンネルidを設定
         addChannelId()
+    }
+
+    //既存のトークンを取得する際の処理
+    override fun onCreate() {
+        val pushToken = FirebaseInstanceId.getInstance().instanceId
+        pushToken.addOnSuccessListener { instanceId ->
+            val token = instanceId.token
+            Log.d(TAG,"push token：$token")
+
+            // サーバにトークンを送信する処理など
+            Log.d(TAG,"onNewToken token:$token")
+            // チャンネルidを設定
+            addChannelId()
+        }
     }
 
     // 通知を受信したときの処理
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        Log.d(TAG,"onMessageReceived token:${message?.data}")
 
         // 今回は通知からタイトルと本文を取得
         val title: String = message?.notification?.title.toString()
@@ -47,8 +70,8 @@ class PushNotificationListenerService: FirebaseMessagingService() {
 
         // 通知メッセージのスタイルを設定（改行表示に対応）
         val inboxStyle = NotificationCompat.InboxStyle()
-        val messageArray: List<String> = text.split("\n")
-        for (msg: String in messageArray) {
+        val messageArray = text.split("\n")
+        for (msg in messageArray) {
             inboxStyle.addLine(msg)
         }
 
@@ -61,9 +84,15 @@ class PushNotificationListenerService: FirebaseMessagingService() {
             .setContentTitle(title)
             .setContentText(text)
             // ステータスバーに表示されるアイコン
-            .setSmallIcon(R.drawable.application_icon)
+            .setSmallIcon(R.drawable.icon_front)
+            //通知エリアに表示されるアイコン
+            .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.application_icon))
             // 上で設定したpendingIntentを設定
             .setContentIntent(pendingIntent)
+            //メッセージをまとめるときに表示するテキスト
+            .setStyle(inboxStyle.setSummaryText("more"))
+            //タップしたら自動で閉じるようにする.
+            .setAutoCancel(true)
             // 優先度を最大化
             .setPriority(PRIORITY_MAX)
             // 通知音を出すように設定
@@ -71,7 +100,8 @@ class PushNotificationListenerService: FirebaseMessagingService() {
 
         // 通知を実施
         // UUIDを付与することで各通知をユニーク化
-        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // 通知エリアに複数表示させるためランダムのIDを設定
         val uuid = UUID.randomUUID().hashCode()
         notificationManager.notify(uuid, notificationBuilder.build())
 
@@ -98,5 +128,9 @@ class PushNotificationListenerService: FirebaseMessagingService() {
             // チャンネル登録
             manager.createNotificationChannel(channel)
         }
+    }
+
+    private fun sendRegistrationToServer(p0:String?){
+        Log.d(TAG,"sendRegistrationTokenToServer($p0)")
     }
 }
