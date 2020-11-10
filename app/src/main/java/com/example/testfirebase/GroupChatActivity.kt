ad886c614b07f6d970aeab287916b71ee34302ea
obakyuu <obakyuu@users.noreply.github.com>
2020-11-10 10:25:23 +0900

    各種コンポーネントと簡易的なイベントを実装
    Signed-off-by: obakyuu <obakyuu@users.noreply.github.com>

--- app/src/main/java/com/example/testfirebase/GroupChatActivity.kt
+++ app/src/main/java/com/example/testfirebase/GroupChatActivity.kt
@@ -2,13 +2,32 @@ package com.example.testfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 
 class GroupChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_chat)
 
+        // 各種コンポーネント
+        val g_recy = findViewById<RecyclerView>(R.id.group_chat_recyclerView)
+        val g_edit = findViewById<EditText>(R.id.group_chat_editText)
+        val g_send = findViewById<Button>(R.id.group_chat_send_button)
+
         // アクションバーの表記を変更
         setTitle("グループチャット")
+
+        // 送信ボタン
+        g_send.setOnClickListener {
+            // テキストボックスに何も記入がないときはスルーする
+            if (g_edit.text.isEmpty()){
+                Toast.makeText(applicationContext, "メッセージを入力してください", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(applicationContext, "メッセージを入力してください", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
