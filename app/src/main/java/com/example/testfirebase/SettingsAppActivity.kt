1d08c3c8bcf480cf5e12eba33e8a7a9fe79cbb11
obakyuu <obakyuu@users.noreply.github.com>
2020-11-28 23:14:41 +0900

    アプリ設定画面を実装したが、おそらく本番ではこの画面は削除される。どちらかというとデバッグ
    Signed-off-by: obakyuu <obakyuu@users.noreply.github.com>

---
+++ app/src/main/java/com/example/testfirebase/SettingsAppActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_app)
+
+
+    }
+}
